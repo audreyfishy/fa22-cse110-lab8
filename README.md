52aa095cb9f9c11979ca7cc9e2c95dcbecd76ffb
audreyfishy <kitazawatakuro@gmail.com>
2022-12-10 21:22:15 -0800

    DONE

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# Lab 8 - Starter
+1. 1 - Since merging the pull requests may break the code, we need to make sure that the code is working properly before merging the pull requests.
+2. No. 
+3. No.
+4. Yes.
+
+![SS](ss.png)
--- __tests__/unit.test.js
+++ __tests__/unit.test.js
@@ -1,5 +1,93 @@
 // unit.test.js
-
 const functions = require('../code-to-unit-test/unit-test-me.js');
 
 // TODO - Part 2
+describe('isPhoneNumber', () => {
+    test('Check valid number', () => {
+        expect(functions.isPhoneNumber('123-456-7890')).toBe(true);
+    });
+
+    test('Check valid number', () => {
+        expect(functions.isPhoneNumber('787-543-5344')).toBe(true);
+    })
+
+    test('Check invalid number', () => {
+        expect(functions.isPhoneNumber('1131234678')).toBe(false);
+    })
+
+    test('Check invalid number', () => {
+        expect(functions.isPhoneNumber('21-221-2-2-2')).toBe(false);
+    })
+})
+
+describe('isEmail', () => {
+    test('Check valid email', () => {
+        expect(functions.isEmail('tkitazawa@ucsd.edu')).toBe(true);
+    });
+
+    test('Check valid email', () => {
+        expect(functions.isEmail('kitazawatakuro@gmail.com')).toBe(true);
+    })
+
+    test('Check invalid email', () => {
+        expect(functions.isEmail('japan.jp')).toBe(false);
+    })
+
+    test('Check invalid email', () => {
+        expect(functions.isEmail('amazon.com')).toBe(false);
+    })
+})
+
+describe('isStrongPassword', () => {
+    test('Check valid password', () => {
+        expect(functions.isStrongPassword('dwFWGA123')).toBe(true);
+    });
+
+    test('Check valid password', () => {
+        expect(functions.isStrongPassword('DiwW42j32')).toBe(true);
+    })
+
+    test('Check invalid password', () => {
+        expect(functions.isStrongPassword('w')).toBe(false);
+    })
+
+    test('Check invalid password', () => {
+        expect(functions.isStrongPassword('?!?!?!??!?!?!')).toBe(false);
+    })
+})
+
+describe('isDate', () => {
+    test('Check valid date', () => {
+        expect(functions.isDate('11/20/2022')).toBe(true);
+    });
+
+    test('Check valid date', () => {
+        expect(functions.isDate('01/01/2000')).toBe(true);
+    })
+
+    test('Check invalid date', () => {
+        expect(functions.isDate('90/334/2022')).toBe(false);
+    })
+
+    test('Check invalid date', () => {
+        expect(functions.isDate('2/2000')).toBe(false);
+    })
+})
+
+describe('isHexColor', () => {
+    test('Check valid hex color', () => {
+        expect(functions.isHexColor('AAA')).toBe(true);
+    });
+
+    test('Check valid hex color', () => {
+        expect(functions.isHexColor('ffffff')).toBe(true);
+    })
+
+    test('Check invalid hex color', () => {
+        expect(functions.isHexColor('1')).toBe(false);
+    })
+
+    test('Check invalid hex color', () => {
+        expect(functions.isHexColor('11')).toBe(false);
+    })
+})
